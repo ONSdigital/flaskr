af2646c4912de295c31f488fa4c3988febce8bc4
Charles <enquiries@movingdigi.com>
2017-01-10 11:06:32 +0000

    removed superfluous stuff

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -1,21 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-#app.run(debug=True)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-from flask import render_template
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
 
 @app.route('/login/')
 def log_in():
     return render_template('login.html')
+
 
 @app.route('/success/')
 def login_success():
